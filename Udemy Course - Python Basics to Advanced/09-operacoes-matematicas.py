7119bc0b382a0018b5bfe840b9ad3bb3a037e2a5
Yuri Ramos <yuri.oliveira.ramos@gmail.com>
2024-09-14 19:35:59 -0300

    notations + exercises

---
+++ Udemy Course - Python Basics to Advanced/09-operacoes-matematicas.py
@@ -0,0 +1,24 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2  #com uma / sempre dá float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2  #com duas // todos os números decimais são ignorados
+print('Divisão Inteira', divisao_inteira) 
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  #resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
