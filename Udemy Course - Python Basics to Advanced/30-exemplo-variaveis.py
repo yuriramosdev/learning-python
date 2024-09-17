2a07357510431688147a7a712ef3b702d0cadbf6
Yuri Ramos <yuri.oliveira.ramos@gmail.com>
2024-09-17 13:00:31 -0300

    notations + exercises

---
+++ Udemy Course - Python Basics to Advanced/30-exemplo-variaveis.py
@@ -0,0 +1,23 @@
+"""
+CONSTANTE = "Variáveis" que não vão mudar
+Muitas condições no mesmo if (ruim)
+     <- Contagem de complexidade (ruim)
+"""
+
+velocidade = 61 #velocidade atual do carro
+local_carro = 90 #local que o carro está na estrada
+
+RADAR_1 = 60 #a velocidade máxima do radar 1
+LOCAL_1 = 100 #local onde o radar 1 está
+RADAR_RANGE = 1 #a distancia onde o radar pega
+
+vel_carro_pass_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
+
+if vel_carro_pass_radar_1:
+    print('Ultrapassou a velocidade limite do Radar.')
+if carro_passou_radar_1:
+    print('Carro passou radar 1')
+if carro_multado_radar_1:
+    print('Carro multado em')
---
+++ Udemy Course - Python Basics to Advanced/31-flag-none-is-isnot-id.py
@@ -0,0 +1,28 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+"""
+
+# v1 = 'a'
+# v2 = 'a'
+# print(id(v1))
+# print(id(v2))
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+if passou_no_if is not None:
+    print('Passou no if')
+    
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
